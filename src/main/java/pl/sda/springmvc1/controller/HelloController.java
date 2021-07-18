405fb61dc9804260364ce96988f2bf345f26bd50
Zdzislaw Trzaska <ztrzaska2@gmail.com>
2021-07-18 11:48:10 +0200

    kliknięcie przycisku

--- src/main/java/pl/sda/springmvc1/controller/HelloController.java
+++ src/main/java/pl/sda/springmvc1/controller/HelloController.java
@@ -1,13 +1,16 @@
 package pl.sda.springmvc1.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/hello")
-    public String page() {
+    public String page(Model model) {
+        int x = 3;
+        x++;
         return "hello";
     }
 }
---
+++ src/main/java/pl/sda/springmvc1/controller/MainController.java
@@ -0,0 +1,19 @@
+package pl.sda.springmvc1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String mainPage(@ModelAttribute("button1") String button,
+                                       Model model) {
+        if(button != null && !button.isEmpty()){
+            model.addAllAttributes("text", "Klieknięto");
+        }
+        return "index";
+    }
+}
